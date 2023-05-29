cb3044da171b8c5dc77b0619b72032e0dcdcca43
francescotroiano98 <francesco-troiano-98@libero.it>
2023-05-29 15:22:21 +0200

    done milestone 0 and 1

---
+++ JS/script.js
@@ -0,0 +1,40 @@
+const myTeamList = [
+       {
+           name: 'Wayne Barnett ',
+           role : 'Founder & CEO',
+           profilePhoto : 'wayne-barnett-founder-ceo.jpg',
+           
+        },
+       {
+            name: 'Angela Caroll',
+            role : 'Chief Editor',
+            profilePhoto : 'angela-caroll-chief-editor.jpg',
+            
+        },
+       {
+            name: 'Walter Gordonl',
+            role : 'Office Manager',
+            profilePhoto : 'walter-gordon-office-manager.jpg',
+            
+        }, 
+        {
+            name: 'Angela Lopez',
+            role : 'Social Media Manager',
+            profilePhoto : 'angela-lopez-social-media-manager.jpg',
+            
+        },
+        {
+            name: 'Scott Estrada',
+            role : 'Developer',
+            profilePhoto : 'scott-estrada-developer.jpg',
+            
+        },
+        {
+            name: 'Barbara Ramos',
+            role : 'Graphic Designer',
+            profilePhoto : 'barbara-ramos-graphic-designer.jpg',
+            
+        },
+
+     ];
+     console.log(myTeamList)
